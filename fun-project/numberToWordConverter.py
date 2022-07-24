612bb8dec1cf8bc762a1f1a12c63d4876a67dc61
J4yM1l <jchrismilton@gmail.com>
2022-07-24 02:04:21 -0400

    Input handles cases from 100 to 999

--- fun-project/numberToWordConverter.py
+++ fun-project/numberToWordConverter.py
@@ -7,13 +7,11 @@ def get_ones_value(num,ones_dic):
 
 def get_tens_value(num,dic_one,dic_two,posOne,posTwo):
     print("{}".format("Tens"))
-    # use cases numbers: 11, 21
     # get the first index value for key values in the dictionary. eg: for num = 20; key: 20, value: twenty; ....
     if num in dic_two:
-        # print("{} is spelt as {}".format(num,dic_one[num].capitalize()
         return dic_two[num][0].capitalize()
     else:
-    # get second index value for key values not dictionary. Range: 11-19. 
+    # get second index value for key values not in dictionary. Range: 11-19. 
     # eg: for key: 11, Value: eleven
         number = posOne * 10
         if posOne <= 1 and posTwo >= 1:
@@ -26,35 +24,36 @@ def get_tens_value(num,dic_one,dic_two,posOne,posTwo):
         return num_in_words.capitalize()
 
 
-def get_hundreds_value(num,onesDic,tensDic,hundredsDic,firstPos,secondPos):
+def get_hundreds_value(num,onesDic,tensDic,hundredsDic,posOne,posTwo):
     print("{}".format("Hundreds"))
-    val = ""
     if num in hundredsDic:
-        # print("{} is spelt as {} {}".format(num, onesDic[1][0].capitalize(),hundredsDic[num]))
-        # elif number_grp
-        return hundredsDic[num]
+        prefix = onesDic[posOne]
+        return f"{prefix} " + hundredsDic[num].capitalize()
     else:
-        if firstPos > 10 and firstPos < 20:            
-            # ones_position = get_ones(firstPos)          
-            # indexPos = get_ones(firstPos) # 110:11 -> 11/10=1 
-            rem = num%100#get_tens(num)
-            # tens = indexPos * 10 + rem
-            indexPos_one = get_ones(firstPos)
-            indexPos_two = get_tens(rem)
-            val = get_ones_value(rem,onesDic,indexPos_one,indexPos_two)
-        else:
-             rem = num%100
-             indexPos_one = get_tens(firstPos)
-             val = get_tens_value(rem,onesDic,tensDic,indexPos_one,secondPos)
-            # number = firstPos * 10
-            # if number > 100:                
-        num_in_words = hundredsDic[100]
-        num_in_words = onesDic[1][0] + f" {num_in_words} and " + val            
-        # print("{} is spelt as {}".format(num,num_in_words.capitalize()))
-        return num_in_words.capitalize()
+        number = posOne * 100
+        if posTwo < 10:
+            if number == 100:
+                num_in_words = hundredsDic[number]
+            else:
+                num_in_words = hundredsDic[100]
+                if posTwo == 0:
+                    prefix = onesDic[posOne]
+                    num_in_words = prefix + f" {num_in_words}"
+                    return num_in_words.capitalize()
+            prefix = onesDic[posOne]
+            num_in_words = prefix + f" {num_in_words} and " + onesDic[posTwo]
+        elif posTwo > 9:
+            num_in_words = hundredsDic[100]
+            prefix = onesDic[posOne]
+            p1 = get_ones(posTwo, 10)
+            p2 = get_tens(posTwo, 10)
+            get_tens_digit = get_tens_value(posTwo,onesDic,tensDic,p1,p2)
+            num_in_words = prefix + f" {num_in_words} and " + get_tens_digit 
+    return num_in_words.capitalize()
 
 def number_to_word(num):
     ones_dic = {
+        0:"zero",
         1:"one",
         2:"two",
         3:"three",
@@ -74,58 +73,38 @@ def number_to_word(num):
         60:["sixty","sixteen"],
         70:["seventy","seventeen"],
         80:["eighty","eighteen"],
-        90:["ninety","nineteen"],       
+        90:["ninety","nineteen"]       
     }
     hundreds_dic = {
         100:"hundred"
     }
-    ones_position = get_ones(num)
-    tens_position = get_tens(num)
-    if ones_position == 0 and tens_position == num:
-        ones_value = get_ones_value(num,ones_dic)
-        print(f"{num} is spelt as {ones_value}")
-    elif ones_position > 0 and tens_position < num:
-            tens_value = get_tens_value(num,ones_dic,tens_dic, ones_position,tens_position)
-            print(f"{num} is spelt as {tens_value}")
-    elif ones_position > 9 and ones_position <100:
-        hundreds_value = get_hundreds_value(num,ones_dic,tens_dic,hundreds_dic,ones_position,tens_position)
-        print(f"{num} is spelt as {hundreds_value}")
+    if num < 100:
+        ones_position = get_ones(num, 10)
+        tens_position = get_tens(num,10)
+        if ones_position == 0 and tens_position == num: # returns tens digits spelt words.
+            ones_value = get_ones_value(num,ones_dic)
+            print(f"{num} is spelt as {ones_value}")
+        elif ones_position > 0 and tens_position < num: # returns tens digits spelt words.
+                tens_value = get_tens_value(num,ones_dic,tens_dic, ones_position,tens_position)
+                print(f"{num} is spelt as {tens_value}")
+    else: 
+        ones_position = get_ones(num, 100)
+        tens_position = get_tens(num,100)
+        if ones_position <1000:
+            hundreds_value = get_hundreds_value(num,ones_dic,tens_dic,hundreds_dic,ones_position,tens_position)
+            print(f"{num} is spelt as {hundreds_value}")
 
-def get_ones(val):
-    ones = val/10
-    # print("remender {}".format(rem))
-    # print("division found index {}".format(int(ones)))
+def get_ones(val,denom):
+    ones = val/denom
     return int(ones)
 
-def get_tens(num):
-    rem = num%10
-    # print(f"remender found val is {rem}")
+def get_tens(num, denom):
+    rem = num%denom
     return int(rem)
 
-# def get_hundreds(num):
-#     rem = num/100
-#     print(f"remender found val is {rem}")
-#     return int(rem)
-
 numb = int(input("Plse Enter a number: "))
-while numb != 0:
+while numb != -1:
     number_to_word(numb)
     numb = int(input("Plse Enter a number: "))
 print("Thank You!")
 print("Goodbye!!")
-
-
-
-'''
-
-1: one -> 1/10=0.1, 1%10=1
-11: eleven -> 11/10=1.1, 11%10=1
-12: twelve -> 12/10=1.2, 12%10=2
-13: thirteen
-14: fourteen
-15: fiftteen
-16: sixteen
-20: twenty -> 20/10=2, 20%10=0
-
-21: twenty one -> 21/10=2.1, 21%10=1
-'''
